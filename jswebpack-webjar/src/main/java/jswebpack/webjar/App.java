a533e375f86e7bcc3eb2df93c7d651890b572d8f
vravikiran <ravikiran.vemula@gmail.com>
2018-07-17 07:07:28 +0530

    Add files via upload

---
+++ jswebpack-webjar/src/main/java/jswebpack/webjar/App.java
@@ -0,0 +1,13 @@
+package jswebpack.webjar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
